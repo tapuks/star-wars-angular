a654e6ced9452deef52bf6017ec2bf0c45f4c85b
Berdiell Sánchez <ddeh@gft.com>
2022-10-27 12:54:58 +0200

    'ok'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
+//Components
 import { AppComponent } from './app.component';
+import { MoviesListComponent } from './components/movies-list/movies-list.component';
+import { CharactersListComponent } from './components/characters-list/characters-list.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { CharacterComponent } from './components/character/character.component';
+import { PageNotFoundComponent } from './components/page-not-found.component';
+
+//Services
+import { MoviesService } from './services/movies.service';
+import { CharactersService } from './services/characters.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesListComponent,
+    CharactersListComponent,
+    MovieComponent,
+    CharacterComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'movies', component: MoviesListComponent },
+      { path: 'characters', component: CharactersListComponent },
+      { path: '', redirectTo: '/characters', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent }
+    ])
   ],
-  providers: [],
+  providers: [
+    MoviesService,
+    CharactersService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
